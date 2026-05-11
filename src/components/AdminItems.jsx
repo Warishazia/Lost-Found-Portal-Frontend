76821e420cfd5c1aa867faa1e0fa963c11b96e27
Warishazia <145816945+Warishazia@users.noreply.github.com>
2026-05-11 22:02:15 +0500

    Update AdminItems.jsx

--- src/components/AdminItems.jsx
+++ src/components/AdminItems.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import api from '../services/api';
+import api from '../services/Api';
 
 const AdminItems = () => {
   const [items, setItems] = useState([]);
